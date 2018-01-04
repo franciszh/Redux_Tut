7fe4c870191af5f3373808ce8c67bcbbba164f44
YiFan Zhao <yifanzhao@192-168-1-121.tpgi.com.au>
2018-01-04 17:42:26 +1100

    Redux action

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList = () => {
         return this.props.books.map(book => {
             return (
-                <li key={book.title} className='list-group-item'>{book.title}</li>
+                <li 
+                key={book.title}
+                onClick={() => this.props.selectBook(book)} 
+                className='list-group-item'>
+                {book.title}
+                </li>
             );
         });
     };
@@ -25,4 +32,10 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anythinhg retured from this function will end up as props
+//on the BookList container
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+//Promote Booklist from a component to a container
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
